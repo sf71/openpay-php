f077bdf9110453f009a0dc2796bb97cfea7be10f
sf71 <scott.gitk@ranch.org>
2021-03-19 19:09:03 -0700

    for PSR-4, class_exists should use autoloader.

--- src/OpenpayApiDerivedResource.php
+++ src/OpenpayApiDerivedResource.php
@@ -13,8 +13,8 @@ class OpenpayApiDerivedResource extends OpenpayApiResourceBase {
 
     private $cacheList = array();
 
-    protected static function getInstance($resourceName, $p = null) {
-        if (class_exists($resourceName . 'List', false)) {
+    protected static function getInstance($resourceName, $props = null) {
+        if (class_exists($resourceName . 'List')) {
             $resource = $resourceName . 'List';
             return new $resource($resourceName);
         }
@@ -35,10 +35,10 @@ class OpenpayApiDerivedResource extends OpenpayApiResourceBase {
         }
     }
 
-    protected function getResource($id) {
-        $id = strtolower($id);
-        if ($this->isResourceListed($id)) {
-            return $this->cacheList[$id];
+    protected function getResource($resourceName) {
+        $resourceName = strtolower($resourceName);
+        if ($this->isResourceListed($resourceName)) {
+            return $this->cacheList[$resourceName];
         }
         return null;
     }
